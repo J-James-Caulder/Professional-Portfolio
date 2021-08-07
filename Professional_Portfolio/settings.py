66d547ecefbc0d73a5ef714d05daf0e98951e463
jjamescaulder <jjamescaulder@gmail.com>
2021-08-07 20:35:23 +0000

    Remove old stuff

--- Professional_Portfolio/settings.py
+++ Professional_Portfolio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$#h!(r1z@#fi7%76d-xaeg2%#ma8u=1%yv^4a3x!oi0y+tazv3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['JJamesCaulder.pythonanywhere.com']
 
 
 # Application definition
@@ -120,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = BASE_DIR / 'static'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
